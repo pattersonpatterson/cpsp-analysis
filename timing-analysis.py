ca55096c746f660d716141253f1d2f2cf6606577
patterson patterson <craze305@gmail.com>
2014-12-01 09:45:03 -0500

    Added possible logic for graphing

--- timing-analysis.py
+++ timing-analysis.py
@@ -10,7 +10,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Take data from excel spreadsheet and put into DataFrame
+# Take data from excel spreadsheet and put into DataFrame (declared globally)
 
 
 # Separate DataFrames into two: one for each release
@@ -24,3 +24,36 @@ import numpy as np
 # y-ticks should be replaced by msg
 # x-label should be milliseconds
 # Plot all on same graph with lines, set line-width=10
+
+
+'''Possible outline:
+for row in dataframe:
+    if start_time < end time:
+        x = np.arange(start_time,end_time+1)
+        y = [row]*len(x)
+    else:
+        x = np.arange(0,start_time+1)
+        x1 = np.arange(end_time,1001)
+        y = [row]*len(x)
+        y1 = [row]*len(x1)
+    
+    if release == old:
+        offset = -0.5
+        color = purple
+    else:
+        offset = 0.5
+        color = blue
+        
+    y = [num+offset for num in y] # possibly send to function
+
+    plt.plot(x and y,color=color,linewidth=10)
+    if(x1 exists):
+        y1 = [num+offset for num in y1]
+        plt.plot(x1 and y1,color=color,linewidth=10)
+        # delete the x1/y1 variables at the end to clean up
+        del x1,y1
+
+        
+# after all rows have been processed, fix the labels and show
+Set the labels to the msg type
+plt.show()'''
