3ffb5118e37db11b68bb2a37fd39bc1036c2b3b1
patterson <craze305@gmail.com>
2014-12-03 13:11:42 -0500

    Completed; fixed bugs; ready for use
    Future: change the data read in so that you only use an excel
    spreadsheet? use args to pick data files?

--- timing-analysis.py
+++ timing-analysis.py
@@ -13,7 +13,8 @@ import csv
 # Take data from csv file and put into list of lists
 # Format: msg#, msg name, start time, end time
 def make_lol(filename):
-    '''Take a csv filename and return a list of lists from the data'''
+    '''Take a csv filename and return a list of lists from the data
+    (Requires import csv)'''
     lol = []
     with open(filename) as csvfile:
         reader = csv.reader(csvfile)
@@ -28,17 +29,20 @@ def make_plots(data,color,offset):
     Format: msg#, msg name, start time, end time
     Also takes a color corresponding to the data to set the data apart
     Also takes numerical offset, moving the data up or down'''
+    
+    # Use i for the y-values    
     i=0
     labels = []
     for num,name,start,end in data:
-        print name,start,end
+        start,end = int(start),int(end) # typecast data as int
+        
+        # Check whether the start time is less than end time; handle accordingly
         if start<end:
-            print name
-            x = np.arange(int(start),int(end)+1)
+            x = np.arange(start,end+1)
             y = [i+offset]*len(x)
         else:
-            x = np.arange(0,int(end)+1)
-            x1 = np.arange(int(start),1001)
+            x = np.arange(0,end+1)
+            x1 = np.arange(start,1001)
             y = [i+offset]*len(x)
             y1 = [i+offset]*len(x1)
         i+=1
@@ -47,20 +51,12 @@ def make_plots(data,color,offset):
             plt.plot(x1,y1,color=color,linewidth=7)
         except NameError:
             pass
+        
+        # Create Label
         labels.append(str(num)+" "+str(name))
     plt.yticks(range(len(labels)),labels)
     
 
-
-
-# Check that the start time is less than the end time
-# If so, create a numpy array from start time to finish
-# If not, create 2 numpy arrays, one from 0 to start, one from finish to 1000
-# Perhaps add these new arrays to DataFrame?
-# y will be index +/- 0.5
-# y-ticks should be replaced by msg
-# x-label should be milliseconds
-# Plot all on same graph with lines, set line-width=10
 
 if __name__ == "__main__":
     old_data = make_lol('./data1.csv')
